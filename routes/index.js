2a0b44d1777669117d01a119c305bc41e40ebb3c
castermild <en.caster.supermild@gmail.com>
2018-05-28 19:05:58 +0900

    implementing

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const logger = require('../logging/logger').appLogger;
 
 const router = express.Router();
 
 router.get('/', (req, res /* , next */) => {
   const count = (req.session.count || 0) + 1;
   req.session.count = count;
-  logger.debug(`debug current count: ${req.session.count}`);
-  logger.info(`info current count: ${req.session.count}`);
-  logger.warn(`warn current count: ${req.session.count}`);
-  logger.error(`error current count: ${req.session.count}`);
   res.render('index', {
     title: 'Name TBD...',
-    uid: req.uid,
     count,
   });
 });
